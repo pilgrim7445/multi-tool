54fe9e804cd57e38e197f4d40bfa5a79dcc6021e
Pilgrim <152723591+pilgrim7445@users.noreply.github.com>
2025-02-06 16:04:46 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
-let sends = 0
-let num = 0
-let stopped = 0
-
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     stopped = 1
 })
-
 input.onButtonPressed(Button.A, function () {
     if (num == 0) {
         num = 1
@@ -13,20 +8,10 @@ input.onButtonPressed(Button.A, function () {
     if (num == 1 && !(stopped == 1)) {
         radio.sendString("YES")
         basic.showString("YES")
+        music.play(music.tonePlayable(494, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         sends += 1
     } else if (num == 2 && !(stopped == 1)) {
         basic.showNumber(sends)
-    }
-})
-
-input.onButtonPressed(Button.B, function () {
-    if (num == 0) {
-        num = 2
-    }
-    if (num == 1 && !(stopped == 1)) {
-        radio.sendString("NO")
-        basic.showString("NO")
-        sends += 1
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -34,18 +19,29 @@ input.onButtonPressed(Button.AB, function () {
         basic.showNumber(sends)
     }
 })
-
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    if (num == 0) {
+        num = 2
+    }
+    if (num == 1 && !(stopped == 1)) {
+        radio.sendString("NO")
+        basic.showString("NO")
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        sends += 1
+    }
+})
+let sends = 0
+let num = 0
+let stopped = 0
 music.play(music.stringPlayable("E B C5 A B G A F ", 360), music.PlaybackMode.InBackground)
 basic.showString("WELCOME")
-
 basic.forever(function () {
     radio.setGroup(1)
     if (num == 2 && !(stopped == 1)) {
         radio.sendString("LOL")
         sends += 1
     }
-})
-
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
 })
